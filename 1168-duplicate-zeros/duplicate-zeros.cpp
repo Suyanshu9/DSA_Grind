0bb2d328144f7fa5462567de7a54cfaa368a77f5
Suryanshu Dobai <113342323+Suyanshu9@users.noreply.github.com>
2026-02-25 19:27:57 +0530

    Time: 0 ms (100.00%) | Memory: 14.2 MB (18.50%) - LeetSync

--- 1168-duplicate-zeros/duplicate-zeros.cpp
+++ 1168-duplicate-zeros/duplicate-zeros.cpp
@@ -2,19 +2,35 @@ class Solution {
 public:
     void duplicateZeros(vector<int>& arr) {
         int n = arr.size();
-        for (int i = 0; i< n;i++){
+        // for (int i = 0; i< n;i++){
+        //     if(arr[i] == 0){
+        //         int temp ;
+        //         if(i != n-1) temp = arr[i+1];
+        //         else break;
+        //         arr[i+1] = 0;
+        //         for(int j = i+2 ; j<n; j++){
+        //             int temp2 = arr[j];
+        //             arr[j] = temp;
+        //             temp = temp2;
+        //         }
+        //         i ++;
+        //     }
+        // }
+        int j = 0;
+        vector<int> ans(n);
+        for(int i = 0; i< n;i++){
+            if (j >= n) break;
             if(arr[i] == 0){
-                int temp ;
-                if(i != n-1) temp = arr[i+1];
-                else break;
-                arr[i+1] = 0;
-                for(int j = i+2 ; j<n; j++){
-                    int temp2 = arr[j];
-                    arr[j] = temp;
-                    temp = temp2;
-                }
-                i ++;
+                ans[j] = 0;
+                if(j+1 < n)
+                ans[j+1] = 0;
+                j += 2;
+            }
+            else{
+                ans[j] = arr[i];
+                j++;
             }
         }
+        arr = ans;
     }
 };
